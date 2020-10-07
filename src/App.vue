705608a0b3d67fe702e882d8ba985418f7ee2233
Author <crypto@exchange.com>
2020-10-07 18:48:02 -0500

    first commit, project kick off

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <px-header :links="links" />
+
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+  </main>
+</template>
+
+<script>
+import PxHeader from '@/components/PxHeader'
+
+export default {
+  name: 'app',
+  components: { PxHeader },
+
+  data() {
+    return {
+      links: [
+        {
+          title: 'BTC',
+          to: { name: 'coin-detail', params: { id: 'bitcoin' } }
+        },
+        {
+          title: 'ETH',
+          to: { name: 'coin-detail', params: { id: 'ethereum' } }
+        },
+        {
+          title: 'LINK',
+          to: { name: 'coin-detail', params: { id: 'chainlink' } }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
