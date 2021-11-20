46378858adf583429267de046a4b31ea5ca5011a
osw16 <oswaldoj16@gmail.com>
2021-11-20 16:52:11 -0500

    actually updating...

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="shadow w-screen">
+  <header class="shadow">
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
@@ -7,7 +7,7 @@
           <router-link
             :to="{ name: 'home' }"
             class="font-semibold text-xl tracking-tight"
-          >Platzi Exchange</router-link>
+          >Fanance Exchange</router-link>
         </div>
         <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-sm lg:flex-grow">
